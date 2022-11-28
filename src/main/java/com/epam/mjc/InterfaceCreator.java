d45c4a87d52d8cb279b55a538a8d2431d13a54d6
NANO <File changed>
2022-11-28 10:58:18 +0600

    1 submission

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,5 +1,7 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
@@ -11,22 +13,64 @@ import java.util.function.Supplier;
 public class InterfaceCreator {
 
     public Predicate<List<String>> isValuesStartWithUpperCase() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        Predicate<List<String>> predicate = strings -> {
+            for (String s : strings) {
+                if (Character.isLowerCase(s.charAt(0)) || Character.isDigit(s.charAt(0))) {
+                    return false;
+                }
+            }
+            return true;
+        };
+        return predicate;
     }
 
     public Consumer<List<Integer>> addEvenValuesAtTheEnd() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        Consumer<List<Integer>> consumer = integers -> {
+            List<Integer> temp = new ArrayList<>();
+            for (Integer i : integers) {
+                if (i % 2 == 0) {
+                    temp.add(i);
+                }
+            }
+            integers.addAll(temp);
+        };
+        return consumer;
     }
 
     public Supplier<List<String>> filterCollection(List<String> values) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        Supplier<List<String>> supplier = () -> {
+            List<String> temp = new ArrayList<>();
+            for (String s:values) {
+                String[] arr = s.split(" ");
+                if(Character.isUpperCase(s.charAt(0)) && s.charAt(s.length()-1)=='.' && arr.length>3){
+                    temp.add(s);
+                }
+            }
+            return temp;
+        };
+        return supplier;
     }
 
     public Function<List<String>, Map<String, Integer>> stringSize() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        Function<List<String>, Map<String, Integer>> function = strings -> {
+            Map<String, Integer> map = new LinkedHashMap<>();
+            for (String s:strings) {
+                map.put(s,s.length());
+            }
+            return map;
+        };
+        return function;
     }
 
     public BiFunction<List<Integer>, List<Integer>, List<Integer>> concatList() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        BiFunction<List<Integer>, List<Integer>, List<Integer>> biFunction = new BiFunction<List<Integer>, List<Integer>, List<Integer>>() {
+            @Override
+            public List<Integer> apply(List<Integer> integers, List<Integer> integers2) {
+                List<Integer> list = new ArrayList<>(integers);
+                list.addAll(integers2);
+                return list;
+            }
+        };
+        return biFunction;
     }
 }
